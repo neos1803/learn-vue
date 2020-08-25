06895741b660ac2c87368f91762ff9fdef16e537
Nanda Purba <oliviergiroud1998@gmail.com>
2020-08-25 23:33:12 +0700

    Add components for search and display list

---
+++ search-app-v2/src/components/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="search mt-8">
+        <input v-model="search" v-on:keyup="checkSearch" class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="tex" placeholder="Insert Title">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchBox",
+    data: function() {
+        return {
+            search: "",
+        }
+    },
+    methods: {
+        checkSearch() {
+            this.$emit("new-search", this.search)
+        }
+    }
+}
+</script>
