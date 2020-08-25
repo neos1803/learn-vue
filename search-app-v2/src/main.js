f352d7c5517e30023f0e766e76c73e5e112a6b01
Nanda Purba <oliviergiroud1998@gmail.com>
2020-08-25 23:34:35 +0700

    Import filters and vue-simple-alert

--- search-app-v2/src/main.js
+++ search-app-v2/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSimpleAlert from 'vue-simple-alert'
+import './filters/price'
 
 Vue.config.productionTip = false
+Vue.use(VueSimpleAlert)
 
 new Vue({
   render: h => h(App),
